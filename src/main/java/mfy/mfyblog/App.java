7d83c97c2ab5785e2705a221499c1b8efd7dbd7e
马凤艳 <2937735461@qq.com>
2018-04-23 20:33:06 +0800

    前端首页面

---
+++ src/main/java/mfy/mfyblog/App.java
@@ -0,0 +1,13 @@
+package mfy.mfyblog;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
